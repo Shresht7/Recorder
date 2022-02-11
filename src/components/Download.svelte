ceeb319cf62d4e638429a4689b91e290008d4694
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-11 14:24:33 +0530

    Style the Download Button

--- src/components/Download.svelte
+++ src/components/Download.svelte
@@ -10,4 +10,20 @@
 {/if}
 
 <style>
+    a {
+        text-decoration: none;
+        font-family: inherit;
+        font-size: var(--btn-font-size, 1rem);
+        color: var(--clr-highlight, white);
+        padding: 0.5em 1em;
+        background-color: var(--clr-primary, black);
+        border: 2px solid var(--clr-primary, black);
+        border-radius: 12px;
+        transition: all 200ms;
+        cursor: pointer;
+    }
+
+    a:hover {
+        transform: scale(1.05);
+    }
 </style>
