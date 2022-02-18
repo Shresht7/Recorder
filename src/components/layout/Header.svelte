5d3960e900681fde67941aa6b1238f348fc9dff0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-18 11:06:42 +0530

    Add Info Button to header

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -11,10 +11,14 @@
 
 <header>
     <div />
-    <Button
-        icon={mode === "light" ? "moon" : "sun"}
-        on:click={() => theme.toggle()}
-    />
+    <div>
+        <Button primary={false}><span>i</span></Button>
+        <Button
+            primary={false}
+            icon={mode === "light" ? "moon" : "sun"}
+            on:click={() => theme.toggle()}
+        />
+    </div>
 </header>
 
 <style>
@@ -24,5 +28,17 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+    }
+
+    div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    span {
+        font-weight: 900;
     }
 </style>
