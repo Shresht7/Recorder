38a55ad6538251babc0eca0d43b5f8a40f5eecae
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-18 11:03:18 +0530

    Add view source to footer

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -1,12 +1,36 @@
 <script lang="ts">
 </script>
 
-<footer />
+<footer>
+    <div />
+    <div>
+        <a href="https://www.github.com/Shresht7/Screen-Recorder">
+            View Source
+        </a>
+    </div>
+</footer>
 
 <style>
     footer {
         width: 100%;
         border-top: 1px solid var(--clr-text, whitesmoke);
         opacity: 0.25;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding-top: 0.25rem;
+    }
+
+    a {
+        color: var(--clr-text, whitesmoke);
+        text-decoration: none;
+        opacity: 0.5;
+        transition: var(--animation-duration, 125ms) ease-in;
+    }
+
+    a:hover,
+    a:focus {
+        opacity: 1;
     }
 </style>
