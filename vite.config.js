8a232d2305065e19385a55760d3304ff980fc5cb
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-19 15:48:56 +0530

    Update PWA Manifest

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     svelte(),
-    VitePWA({})
+    VitePWA({
+      includeAssets: [
+        // 'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        "name": "Screen-Recorder",
+        "short_name": "Screen-Recorder",
+        "description": "A simple screen-recorder powered by the web!",
+        "theme_color": "#E87461",
+        "background_color": "#E87461",
+        "icons": [
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ],
+        "display": "standalone"
+      }
+    })
   ]
 })
