05411a86f175b2c306a57f6ecd828f64593929f9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-19 19:58:48 +0530

    Disabled PWA for development

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,45 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    VitePWA({
-      includeAssets: [
-        // 'favicon.svg',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png'
-      ],
-      manifest: {
-        name: "Screen-Recorder",
-        short_name: "Screen-Recorder",
-        description: "A simple screen-recorder powered by the web!",
-        theme_color: "#E87461",
-        background_color: "#E87461",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ],
-        display: "standalone"
-      }
-    })
+    // VitePWA({
+    //   mode: process.env.NODE_ENV || 'development',
+    //   includeAssets: [
+    //     // 'favicon.svg',
+    //     'favicon.ico',
+    //     'robots.txt',
+    //     'apple-touch-icon.png'
+    //   ],
+    //   manifest: {
+    //     name: "Screen-Recorder",
+    //     short_name: "Screen-Recorder",
+    //     description: "A simple screen-recorder powered by the web!",
+    //     theme_color: "#E87461",
+    //     background_color: "#E87461",
+    //     icons: [
+    //       {
+    //         src: "pwa-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png"
+    //       },
+    //       {
+    //         src: "pwa-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png"
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any maskable',
+    //       }
+    //     ],
+    //     display: "standalone"
+    //   }
+    // })
   ]
 })
