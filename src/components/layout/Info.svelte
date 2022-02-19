dd2fc0ce5dd4a47ecd4a2324909ad17d8e969dd2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-19 11:40:34 +0530

    Update info message

--- src/components/layout/Info.svelte
+++ src/components/layout/Info.svelte
@@ -15,7 +15,47 @@
     </div>
 
     <div>
-        <p>Screen-Recorder powered by web technologies!</p>
+        <p>A simple screen-recorder powered by the web!</p>
+
+        <h2>Features</h2>
+
+        <ul>
+            <li>Powered by the web! Runs everywhere on a browser.</li>
+            <li>No sign-up or downloads required.</li>
+            <li>Installable as a Progressive-Web-App (PWA). Works offline!</li>
+            <li>
+                100% Client-sided. No ads, no trackers, no server interaction at
+                all.
+            </li>
+        </ul>
+
+        <h2>Usage</h2>
+
+        <ol>
+            <li>
+                1. Click on the <span>Select Screen</span> button. Your browser will
+                prompt you to select the screen that you want to share with this
+                app.
+            </li>
+            <li>
+                2. Once a screen has been selected, a preview of the video
+                stream will be shown. Click <span>Record</span> to start recording
+                the screen.
+            </li>
+            <li>
+                3. You can <span>Pause</span> and <span>Continue</span> the
+                recording process whenever you wish. When you're done, click the
+                <span>Stop</span> button. This will stop the recording process and
+                show you the recorded video.
+            </li>
+            <li>
+                4. Click on <span>Download</span> to save the recording to your device.
+            </li>
+            <li>
+                Click on <span>Reset</span> or <span>Discard</span> to discard your
+                recording at any stage.
+            </li>
+        </ol>
     </div>
 
     <div slot="footer" />
@@ -24,6 +64,10 @@
 <style>
     h1 {
         text-align: center;
+    }
+
+    h2 {
+        margin: 1rem 0;
     }
 
     .header {
@@ -35,5 +79,9 @@
         padding-bottom: 0.5rem;
         margin-bottom: 1rem;
         border-bottom: 2px solid rgba(255, 255, 255, 0.33);
+    }
+
+    li span {
+        color: var(--clr-primary, orangered);
     }
 </style>
