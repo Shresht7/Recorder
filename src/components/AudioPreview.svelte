b74737e6c49ce664ec72f2d6b2fe4ef35549e390
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-27 01:41:18 +0530

    Create audio recorder component

---
+++ src/components/AudioPreview.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { getAllSupportedMimeTypes } from "../helpers";
+
+    import download from "../library/download";
+
+    import { onMount } from "svelte";
+    import options from "../library/options";
+
+    onMount(() => {
+        options.set({ mimeType: getAllSupportedMimeTypes("audio")[0] });
+    });
+</script>
+
+<audio src={$download.href} controls />
+
+<style></style>
--- src/components/ScreenRecorder/ScreenRecorder.svelte
+++ src/components/ScreenRecorder/ScreenRecorder.svelte
@@ -3,17 +3,26 @@
     import Recorder from "../Recorder.svelte";
     import ControlPanel from "../ControlPanel/ControlPanel.svelte";
     import VideoPreview from "../VideoPreview/VideoPreview.svelte";
+    import AudioPreview from "../AudioPreview.svelte";
     import NotSupported from "./NotSupported.svelte";
 
     /** Screen Capture API are supported by the user's device */
     const screenCaptureSupported =
         typeof navigator?.mediaDevices?.getDisplayMedia === "function";
+
+    import stream from "../../library/stream";
+    let isVideoTrack: boolean = true;
+    $: isVideoTrack = $stream.getVideoTracks().length > 0;
 </script>
 
 {#if screenCaptureSupported}
     <Recorder>
         <ControlPanel />
-        <VideoPreview />
+        {#if isVideoTrack}
+            <VideoPreview />
+        {:else}
+            <AudioPreview />
+        {/if}
     </Recorder>
 {:else}
     <NotSupported />
