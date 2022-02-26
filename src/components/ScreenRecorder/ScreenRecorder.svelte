481737af52be11557b88cbebb23c905887245b23
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-26 15:00:23 +0530

    Move ScreenRecorder components to their own folder

--- src/components/ScreenRecorder/ScreenRecorder.svelte
+++ src/components/ScreenRecorder/ScreenRecorder.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     //  Components
     import NotSupported from "./NotSupported.svelte";
-    import ControlPanel from "./ControlPanel/ControlPanel.svelte";
-    import VideoPreview from "./VideoPreview.svelte";
+    import ControlPanel from "../ControlPanel/ControlPanel.svelte";
+    import VideoPreview from "../VideoPreview.svelte";
 
     /** Screen Capture API are supported by the user's device */
     const screenCaptureSupported =
