776951b0acdeddf0f646da64b61dde299d92d450
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-27 11:13:13 +0530

    Fix: move Audio folder inside components

--- src/components/Audio/AudioPreview.svelte
+++ src/components/Audio/AudioPreview.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import { getAllSupportedMimeTypes } from "../helpers";
+    import { getAllSupportedMimeTypes } from "../../helpers";
 
-    import download from "../library/download";
+    import download from "../../library/download";
 
     import { onMount } from "svelte";
-    import options from "../library/options";
+    import options from "../../library/options";
 
     onMount(() => {
         options.set({ mimeType: getAllSupportedMimeTypes("audio")[0] });
