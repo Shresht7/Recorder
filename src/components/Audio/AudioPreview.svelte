a751db24eddbea10501dfd1cac526e59b3f2e324
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-27 13:34:30 +0530

    Improve Audio Preview

--- src/components/Audio/AudioPreview.svelte
+++ src/components/Audio/AudioPreview.svelte
@@ -1,16 +1,46 @@
 <script lang="ts">
+    //  Components
+    import Icon from "../utility/Icon.svelte";
+
+    //  Helpers
     import { getAllSupportedMimeTypes } from "../../helpers";
 
+    //  Stores
     import download from "../../library/download";
-
-    import { onMount } from "svelte";
+    import { state } from "../../library/recording";
     import options from "../../library/options";
+
+    //  Library
+    import { onMount } from "svelte";
 
     onMount(() => {
         options.set({ mimeType: getAllSupportedMimeTypes("audio")[0] });
     });
 </script>
 
-<audio src={$download.href} controls />
+<div class="container">
+    {#if $download.href}
+        <audio src={$download.href} controls />
+    {:else}
+        <Icon
+            name="mic"
+            size="5rem"
+            color={$state === "recording" ? "red" : "default"}
+        />
+    {/if}
+</div>
 
-<style></style>
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+    }
+
+    audio {
+        width: 75vw;
+    }
+</style>
